bbf788611f85387ecfee3ab4b6669bc065175315
Justin Bezanson <justinbezanson@gmail.com>
2025-11-23 14:32:29 -0400

    test app

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
+import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	adapter: node({
+    mode: 'standalone',
+  }),
+  vite: {
+	plugins: [tailwindcss()],
+  },
+});	
